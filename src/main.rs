4df68d4f554889267f7f63d492b8425e1a7fd4ae
CherryKitten <git@cherrykitten.dev>
2023-01-29 14:16:28 +0100

    Add traversal of music library

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+use std::{fs, io};
+use std::fs::DirEntry;
+use std::path::Path;
+
+fn main() -> Result<(), io::Error> {
+    let test_path = Path::new("test_data/music");
+    println!("Hello, {}!", test_path.display());
+    traverse_dir(test_path);
+    Ok(())
+}
+
+fn traverse_dir(dir: &Path) -> io::Result<()> {
+    if dir.is_dir() {
+        for entry in fs::read_dir(dir)? {
+            let entry = entry?;
+            let path = entry.path();
+
+            if path.is_dir() {
+                traverse_dir(&path);
+            } else {
+                println!("{:?}", path);
+            }
+        }
+    }
+    Ok(())
 }
